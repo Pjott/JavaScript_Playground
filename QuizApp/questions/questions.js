75f59220f6b353b7fc863ea78643e6466b704393
Piotr Bala <piotr.baa@gmail.com>
2017-09-16 18:28:06 +0200

    Initial commit and partially done quiz app (work in progress)

---
+++ QuizApp/questions/questions.js
@@ -0,0 +1,22 @@
+var questions = [
+	{
+		question : "Który z tych płazów jest największy?",
+		options : ["Wąż boa", "Waran", "Żaba"],
+		answer: "Żaba"
+	},
+	{
+		question : "Który miesiąc ma 28 dni?",
+		options : ["Luty", "Luty w roku przestępnym", "Każdy miesiąc"],
+		answer: "Luty"
+	},
+	{
+		question : "Malarz miał namalować na drzwiach numery od 1 do 100. Ile dziewiątek musiał namalować?",
+		options : ["1", "11", "20"],
+		answer: "11"
+	},
+	{
+		question : "Każdy kij ma dwa końce. Ile końców ma 7,5 kija?",
+		options : ["7,5", "15", "16"],
+		answer: "16"
+	}
+];
